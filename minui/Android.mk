38058dc13ce2160d8baff52e9adda41bc96a0787
preludedrew <andrew.boren@gmail.com>
2011-01-29 23:30:44 -0700

    first commit

---
+++ minui/Android.mk
@@ -0,0 +1,20 @@
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := graphics.c events.c resources.c
+
+LOCAL_C_INCLUDES +=\
+    external/libpng\
+    external/zlib
+
+ifneq ($(BOARD_LDPI_RECOVERY),)
+    LOCAL_CFLAGS += -DBOARD_LDPI_RECOVERY='"$(BOARD_LDPI_RECOVERY)"'
+endif
+
+ifneq ($(BOARD_HAS_JANKY_BACKBUFFER),)
+    LOCAL_CFLAGS += -DBOARD_HAS_JANKY_BACKBUFFER
+endif
+
+LOCAL_MODULE := libminui
+
+include $(BUILD_STATIC_LIBRARY)
