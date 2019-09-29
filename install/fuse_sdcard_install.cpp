63f1a8441fa539c3730db37ce4ad4aa80db7f8c1
Alessandro Astone <ales.astone@gmail.com>
2019-09-29 17:56:49 +0200

    recovery: sdcard is data/media/0
    Change-Id: Ie9978ac8b526c11199f72c4362ae3313218b48d4

--- install/fuse_sdcard_install.cpp
+++ install/fuse_sdcard_install.cpp
@@ -38,7 +38,7 @@
 #include "install/install.h"
 #include "otautil/roots.h"
 
-static constexpr const char* SDCARD_ROOT = "/sdcard";
+static constexpr const char* SDCARD_ROOT = "/data/media/0";
 // How long (in seconds) we wait for the fuse-provided package file to
 // appear, before timing out.
 static constexpr int SDCARD_INSTALL_TIMEOUT = 10;
@@ -131,7 +131,7 @@ static bool StartSdcardFuse(const std::string& path) {
 
   // The installation process expects to find the sdcard unmounted. Unmount it with MNT_DETACH so
   // that our open file continues to work but new references see it as unmounted.
-  umount2("/sdcard", MNT_DETACH);
+  umount2("/data", MNT_DETACH);
 
   return run_fuse_sideload(std::move(file_data_reader)) == 0;
 }
